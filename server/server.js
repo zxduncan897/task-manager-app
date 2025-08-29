01226556b28d21050c931e99ee31121c686b7a43
john riley <jremail@yahoo.com>
2025-08-29 01:27:08 -0700

    fix:  corrected CORS middleware code problem

--- server/server.js
+++ server/server.js
@@ -8,14 +8,13 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
+
 app.use(cors());
 app.use(express.json());
 app.use('/tasks', taskRoutes);
+
 // Connect to MongoDB (replace with your connection string)
-
-
-//mongoose.connect('mongodb://localhost:27017/taskmanager', {
- mongoose.connect('process.env.MONGODB_URI || 'mongodb://localhost:27017/taskmanager'', {
+mongoose.connect('mongodb://localhost:27017/taskmanager', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
